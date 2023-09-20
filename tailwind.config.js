cad4bbea89fbe799c1d29fe24975e48a2aa71039
Lord Ghapa <felipef32@hotmail.com>
2023-09-19 23:27:11 -0300

    ✨ Feat: create sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,7 +42,11 @@ export default {
       backgroundImage: {
         'hero-pattern':
           "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')"
+      },
+      flex: {
+        '2': '2 2 0%'
       }
+
     }
   },
   plugins: []
