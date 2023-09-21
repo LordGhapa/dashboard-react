2f834b166625dde2e7efe8abb9c4122bd02c9927
Lord Ghapa <felipef32@hotmail.com>
2023-09-21 02:42:16 -0300

    ✨ Feat: create app Calendar

--- src/data/dummy.jsx
+++ src/data/dummy.jsx
@@ -2716,197 +2716,198 @@ export const ordersData = [
 export const scheduleData = [
   {
     Id: 1,
-    Subject: 'Explosion of Betelgeuse Star',
-    Location: 'Space Center USA',
+    Subject: 'Explosão da Estrela Betelgeuse',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-10T04:00:00.000Z',
     EndTime: '2021-01-10T05:30:00.000Z',
     CategoryColor: '#1aaa55'
   },
   {
     Id: 2,
-    Subject: 'Thule Air Crash Report',
-    Location: 'Newyork City',
+    Subject: 'Relatório de Acidente Aéreo de Thule',
+    Location: 'Cidade de Nova York',
     StartTime: '2021-01-11T06:30:00.000Z',
     EndTime: '2021-01-11T08:30:00.000Z',
     CategoryColor: '#357cd2'
   },
   {
     Id: 3,
-    Subject: 'Blue Moon Eclipse',
-    Location: 'Space Center USA',
+    Subject: 'Eclipse da Lua Azul',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-12T04:00:00.000Z',
     EndTime: '2021-01-12T05:30:00.000Z',
     CategoryColor: '#7fa900'
   },
   {
     Id: 4,
-    Subject: 'Meteor Showers in 2021',
-    Location: 'Space Center USA',
+    Subject: 'Chuvas de Meteoros em 2021',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-13T07:30:00.000Z',
     EndTime: '2021-01-13T09:00:00.000Z',
     CategoryColor: '#ea7a57'
   },
   {
     Id: 5,
-    Subject: 'Milky Way as Melting pot',
-    Location: 'Space Center USA',
+    Subject: 'Via Láctea como Ponto de Fusão',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-14T06:30:00.000Z',
     EndTime: '2021-01-14T08:30:00.000Z',
     CategoryColor: '#00bdae'
   },
   {
     Id: 6,
-    Subject: 'Mysteries of Bermuda Triangle',
-    Location: 'Bermuda',
+    Subject: 'Mistérios do Triângulo das Bermudas',
+    Location: 'Bermudas',
     StartTime: '2021-01-14T04:00:00.000Z',
     EndTime: '2021-01-14T05:30:00.000Z',
     CategoryColor: '#f57f17'
   },
   {
     Id: 7,
-    Subject: 'Glaciers and Snowflakes',
-    Location: 'Himalayas',
+    Subject: 'Glaciares e Flocos de Neve',
+    Location: 'Himalaias',
     StartTime: '2021-01-15T05:30:00.000Z',
     EndTime: '2021-01-15T07:00:00.000Z',
     CategoryColor: '#1aaa55'
   },
   {
     Id: 8,
-    Subject: 'Life on Mars',
-    Location: 'Space Center USA',
+    Subject: 'Vida em Marte',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-16T03:30:00.000Z',
     EndTime: '2021-01-16T04:30:00.000Z',
     CategoryColor: '#357cd2'
   },
   {
     Id: 9,
-    Subject: 'Alien Civilization',
-    Location: 'Space Center USA',
+    Subject: 'Civilização Alienígena',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-18T05:30:00.000Z',
     EndTime: '2021-01-18T07:30:00.000Z',
     CategoryColor: '#7fa900'
   },
   {
     Id: 10,
-    Subject: 'Wildlife Galleries',
-    Location: 'Africa',
+    Subject: 'Galerias de Vida Selvagem',
+    Location: 'África',
     StartTime: '2021-01-20T05:30:00.000Z',
     EndTime: '2021-01-20T07:30:00.000Z',
     CategoryColor: '#ea7a57'
   },
   {
     Id: 11,
-    Subject: 'Best Photography 2021',
-    Location: 'London',
+    Subject: 'Melhores Fotografias de 2021',
+    Location: 'Londres',
     StartTime: '2021-01-21T04:00:00.000Z',
     EndTime: '2021-01-21T05:30:00.000Z',
     CategoryColor: '#00bdae'
   },
   {
     Id: 12,
-    Subject: 'Smarter Puppies',
-    Location: 'Sweden',
+    Subject: 'Filhotes Mais Inteligentes',
+    Location: 'Suécia',
     StartTime: '2021-01-08T04:30:00.000Z',
     EndTime: '2021-01-08T06:00:00.000Z',
     CategoryColor: '#f57f17'
   },
   {
     Id: 13,
-    Subject: 'Myths of Andromeda Galaxy',
-    Location: 'Space Center USA',
+    Subject: 'Mitologia da Galáxia de Andrômeda',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-06T05:00:00.000Z',
     EndTime: '2021-01-06T07:00:00.000Z',
     CategoryColor: '#1aaa55'
   },
   {
     Id: 14,
-    Subject: 'Aliens vs Humans',
-    Location: 'Research Center of USA',
+    Subject: 'Alienígenas vs Humanos',
+    Location: 'Centro de Pesquisa dos EUA',
     StartTime: '2021-01-05T04:30:00.000Z',
     EndTime: '2021-01-05T06:00:00.000Z',
     CategoryColor: '#357cd2'
   },
   {
     Id: 15,
-    Subject: 'Facts of Humming Birds',
-    Location: 'California',
+    Subject: 'Fatos sobre Beija-Flores',
+    Location: 'Califórnia',
     StartTime: '2021-01-19T04:00:00.000Z',
     EndTime: '2021-01-19T05:30:00.000Z',
     CategoryColor: '#7fa900'
   },
   {
     Id: 16,
-    Subject: 'Sky Gazers',
-    Location: 'Alaska',
+    Subject: 'Observadores do Céu',
+    Location: 'Alasca',
     StartTime: '2021-01-22T05:30:00.000Z',
     EndTime: '2021-01-22T07:30:00.000Z',
     CategoryColor: '#ea7a57'
   },
   {
     Id: 17,
-    Subject: 'The Cycle of Seasons',
-    Location: 'Research Center of USA',
+    Subject: 'O Ciclo das Estações',
+    Location: 'Centro de Pesquisa dos EUA',
     StartTime: '2021-01-11T00:00:00.000Z',
     EndTime: '2021-01-11T02:00:00.000Z',
     CategoryColor: '#00bdae'
   },
   {
     Id: 18,
-    Subject: 'Space Galaxies and Planets',
-    Location: 'Space Center USA',
+    Subject: 'Galáxias e Planetas Espaciais',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-11T11:30:00.000Z',
     EndTime: '2021-01-11T13:00:00.000Z',
-    CategoryColor: '#f57f17'
-  },
-  {
+    CategoryColor: '#00bdae'
+  } , {
     Id: 19,
-    Subject: 'Lifecycle of Bumblebee',
-    Location: 'San Fransisco',
+    Subject: 'Ciclo de Vida do Bombus',
+    Location: 'San Francisco',
     StartTime: '2021-01-14T00:30:00.000Z',
     EndTime: '2021-01-14T02:00:00.000Z',
     CategoryColor: '#7fa900'
   },
   {
     Id: 20,
-    Subject: 'Alien Civilization',
-    Location: 'Space Center USA',
+    Subject: 'Civilização Alienígena',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-14T10:30:00.000Z',
     EndTime: '2021-01-14T12:30:00.000Z',
     CategoryColor: '#ea7a57'
   },
   {
     Id: 21,
-    Subject: 'Alien Civilization',
-    Location: 'Space Center USA',
+    Subject: 'Civilização Alienígena',
+    Location: 'Space Center EUA',
     StartTime: '2021-01-10T08:30:00.000Z',
     EndTime: '2021-01-10T10:30:00.000Z',
     CategoryColor: '#ea7a57'
   },
   {
     Id: 22,
-    Subject: 'The Cycle of Seasons',
-    Location: 'Research Center of USA',
+    Subject: 'O Ciclo das Estações',
+    Location: 'Centro de Pesquisa dos EUA',
     StartTime: '2021-01-12T09:00:00.000Z',
     EndTime: '2021-01-12T10:30:00.000Z',
     CategoryColor: '#00bdae'
   },
   {
     Id: 23,
-    Subject: 'Sky Gazers',
-    Location: 'Greenland',
+    Subject: 'Observadores do Céu',
+    Location: 'Groenlândia',
     StartTime: '2021-01-15T09:00:00.000Z',
     EndTime: '2021-01-15T10:30:00.000Z',
     CategoryColor: '#ea7a57'
   },
   {
     Id: 24,
-    Subject: 'Facts of Humming Birds',
-    Location: 'California',
+    Subject: 'Fatos sobre Beija-Flores',
+    Location: 'Califórnia',
     StartTime: '2021-01-16T07:00:00.000Z',
     EndTime: '2021-01-16T09:00:00.000Z',
     CategoryColor: '#7fa900'
   }
-]
+];
+
+
 
 export const lineChartData = [
   [
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,51 @@
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop
+} from '@syncfusion/ej2-react-schedule'
+
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
+import { scheduleData } from '../data/dummy'
+import { Header } from '../components'
+
 const Calendar = () => {
-  return ( <div>Calendar</div> );
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl ">
+      <Header title={'Calendário'} category={'App'} />
+      <ScheduleComponent
+        height={'650px'}
+        eventSettings={{
+          dataSource: scheduleData,
+
+          enableTooltip: true,
+          allowDeleting: true
+        }}
+        currentView="Week"
+        allowInline
+      
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <ViewsDirective >
+          <ViewDirective option="Day" displayName="Dia" />
+          <ViewDirective option="Week" displayName="Semana" />
+          <ViewDirective option="WorkWeek" displayName="Dia Util" />
+          <ViewDirective option="Month" displayName="Mês" />
+          <ViewDirective option="Agenda" displayName="Agenda" />
+        </ViewsDirective>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  )
 }
- 
-export default Calendar;
+
+export default Calendar
