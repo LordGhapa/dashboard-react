a655e1ed72051d914f9283e70508cb8abfbf6633
Lord Ghapa <felipef32@hotmail.com>
2023-09-22 03:12:34 -0300

    ✨ Feat: create line chart

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,8 +1,14 @@
-
+import { ChartsHeader, Header, LineChart } from '../../components'
 
 function Line() {
   return (
-    <div>Line</div>
+    <div className="m-4 md:m-10 mt-24 p-2 p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg">
+      <Header category={'Taxa de inflação'} title={'Gráfico'} />
+
+      <div className="w-full ">
+        <LineChart />
+      </div>
+    </div>
   )
 }
 
